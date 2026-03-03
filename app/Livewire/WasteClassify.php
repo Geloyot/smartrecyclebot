610feec3dfb4da53aabf7fedb1819e174de28d7d
Geloyot <johnangeloayson@gmail.com>
2026-03-03 19:30:00 +0800

    Update WasteClassify

--- app/Livewire/WasteClassify.php
+++ app/Livewire/WasteClassify.php
@@ -36,8 +36,8 @@ class WasteClassify extends Component
 
         $this->stats = [
             'total_today' => WasteObject::where('created_at', '>=', $today)->count(),
-            'biodegradable' => WasteObject::where('classification', 'Biodegradable')->count(),
-            'non_biodegradable' => WasteObject::where('classification', 'Non-Biodegradable')->count(),
+            'biodegradable' => WasteObject::where('classification', 'ILIKE', 'Biodegradable')->count(),
+            'non_biodegradable' => WasteObject::where('classification', 'ILIKE', 'Non-Biodegradable')->count(),
         ];
     }
 
